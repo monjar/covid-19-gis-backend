8199c19ce5477f22840b8703769fd953ca731a08
Amirali Monjar <monjaramirali@gmail.com>
2020-03-28 16:45:01 +0430

    manual for main page added.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const gisRouter = require("./routers/gis-controller");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT;
@@ -8,5 +9,12 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 app.use("/gis", gisRouter);
+
+app.use(
+  "/",
+  express.Router().get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/views/index.html"));
+  })
+);
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
