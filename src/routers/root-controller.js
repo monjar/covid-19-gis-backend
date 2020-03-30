56b2a21ba50599f9c78be016faeb303ec0d8aa19
Amirali Monjar <monjaramirali@gmail.com>
2020-03-30 15:49:32 +0430

    main page display bug fixed

--- src/routers/root-controller.js
+++ src/routers/root-controller.js
@@ -4,7 +4,7 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
+  res.sendFile(path.join(__dirname + "/../views/index.html"));
 });
 
 module.exports = router;
